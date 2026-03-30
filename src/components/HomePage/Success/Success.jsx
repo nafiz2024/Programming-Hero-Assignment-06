6ea784f970274a198616fb5b7b07b3bbdecfd812
Nafiz Alam <nafizalam.dev@gmail.com>
2026-03-31 04:17:40 +0600

    Some Issue Fix

--- src/components/HomePage/Success/Success.jsx
+++ src/components/HomePage/Success/Success.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Success = () => {
     return (
         <div className='bg-gradient-to-tr from-[#4F39F6] to-[#9514FA]'>
-            <div className="container mx-auto py-15 px-5 lg:px-0 grid grid-cols-1 lg:grid-cols-3 text-center divide-x divide-white">
+            <div className="container mx-auto py-15 px-5 lg:px-0 grid grid-cols-1 lg:grid-cols-3 text-center lg:divide-x divide-white gap-10 lg:gap-0">
                 <div className="">
                     <h1 className='text-6xl font-extrabold text-white'>50K+</h1>
                     <p className='text-2xl font-medium text-white'>Active Users</p>
